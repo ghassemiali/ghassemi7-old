612880984eb43f3d588b2beba254b91ad071f0de
ghassemiali <agh101981@gmail.com>
2022-01-03 22:17:40 +0330

    6-2

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 from blog.models import Post
 
 def blog_home(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(status=1)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
