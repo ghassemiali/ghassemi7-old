27134661b7dff407707bc054955659bd756fbdc3
ghassemiali <agh101981@gmail.com>
2022-01-03 23:56:03 +0330

    add number of views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 
+def add_one_in_views(post):
+    post.counted_views += 1
+    post.save()
+
 def blog_home(request):
     posts = Post.objects.filter(status=1)
     context = {'posts': posts}
@@ -9,6 +13,7 @@ def blog_home(request):
 def blog_single(request, pid):
     post = get_object_or_404(Post, pk=pid)
     context = {'post': post}
+    add_one_in_views(post)
     return render(request, 'blog/blog-single.html', context)
     
 def test_view(request, pid):
