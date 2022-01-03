2102621696f44df4ac7d6a9ac1ffd2a552b3eb38
ghassemiali <agh101981@gmail.com>
2022-01-03 22:06:34 +0330

    6-1

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from blog.views import blog_home, blog_single
+from blog.views import blog_home, blog_single, test_view
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', blog_home, name='home'),
     path('single/', blog_single, name='single'),
+    path('test/', test_view, name='test')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def blog_home(request):
     return render(request, 'blog/blog-home.html')
@@ -6,3 +7,7 @@ def blog_home(request):
 def blog_single(request):
     return render(request, 'blog/blog-single.html')
     
+def test_view(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/test.html', context)
