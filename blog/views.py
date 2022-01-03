1afe8201295eb06942f620b14b3656551fe885b6
ghassemiali <agh101981@gmail.com>
2022-01-04 00:05:16 +0330

    6-5

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from blog.models import Post
+import datetime
 
 def add_one_in_views(post):
     post.counted_views += 1
     post.save()
 
 def blog_home(request):
-    posts = Post.objects.filter(status=1)
+    x = datetime.timedelta(hours=3, minutes=30)
+    now = datetime.datetime.now().astimezone(datetime.timezone(x))
+    posts = Post.objects.filter(status=1, published_date__lte=now)    
     context = {'posts': posts}
     return render(request, 'blog/blog-home.html', context)
 
 def blog_single(request, pid):
-    post = get_object_or_404(Post, pk=pid)
+    x = datetime.timedelta(hours=3, minutes=30)
+    now = datetime.datetime.now().astimezone(datetime.timezone(x))
+    posts = Post.objects.filter(status=1, published_date__lte=now)     
+    post = get_object_or_404(posts, pk=pid)
     context = {'post': post}
     add_one_in_views(post)
     return render(request, 'blog/blog-single.html', context)
