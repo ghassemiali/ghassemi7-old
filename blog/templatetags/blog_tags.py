9217ddde3862d885f5b1207ace6aaddf11a07f06
ghassemiali <agh101981@gmail.com>
2022-01-05 18:59:42 +0330

    7-2 simple tags

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,23 @@
 from django import template
+from blog.models import Post
 
 register = template.Library()
+
+@register .simple_tag
+def hello():
+    return ("hello")
+
+@register .simple_tag(name='dardo')
+def function(input_number = 3):
+    return input_number * 2
+
+@register .simple_tag(name='totalposts')
+def function_2():
+    posts = Post.objects.filter(status=1).count()
+    return posts
+
+@register .simple_tag(name='allposts')
+def function_3():
+    posts = Post.objects.filter(status=1)
+    return posts
 
