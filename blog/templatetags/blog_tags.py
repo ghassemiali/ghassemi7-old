d12ea9a824d9db2605815e5a2f731c61358bc294
ghassemiali <agh101981@gmail.com>
2022-01-07 01:33:23 +0330

    Update blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -3,25 +3,25 @@ from blog.models import Post
 
 register = template.Library()
 
-@register .simple_tag
+@register.simple_tag
 def hello():
     return ("hello")
 
-@register .simple_tag(name='dardo')
+@register.simple_tag(name='dardo')
 def function(input_number = 3):
     return input_number * 2
 
-@register .simple_tag(name='totalposts')
+@register.simple_tag(name='totalposts')
 def function_2():
     posts = Post.objects.filter(status=1).count()
     return posts
 
-@register .simple_tag(name='allposts')
+@register.simple_tag(name='allposts')
 def function_3():
     posts = Post.objects.filter(status=1)
     return posts
 
-@register .filter
+@register.filter
 def snippet(value, arg=15):
     return value[:arg] + '...'
 
